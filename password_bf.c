4b0f610eba3c4d9cd3b2f07f5cc8160c964bd24e
Thiago Parisotto Dias <thiagodias158@hotmail.com>
2023-11-16 21:48:26 -0300

    ajustes

--- password_bf.c
+++ password_bf.c
@@ -14,87 +14,87 @@ char letters[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890";
  * Print a digest of MD5 hash.
 */
 void print_digest(byte * hash){
-	int x;
+    int x;
 
-	for(x = 0; x < MD5_DIGEST_LENGTH; x++)
-        	printf("%02x", hash[x]);
-	printf("\n");
+    for(x = 0; x < MD5_DIGEST_LENGTH; x++)
+            printf("%02x", hash[x]);
+    printf("\n");
 }
 
 /*
  * This procedure generate all combinations of possible letters
 */
 void iterate(byte * hash1, byte * hash2, char *str, int idx, int len, int *ok) {
-    printf("iterating string = %s, int = %d, length = %d, ok = %d", str, idx, len, ok);
-	int c;
-	// 'ok' determines when the algorithm matches.
-	if(*ok) return;
-	if (idx < (len - 1)) {
-		// Iterate for all letter combination.
-		for (c = 0; c < strlen(letters) && *ok==0; ++c) {
-			str[idx] = letters[c];
-			// Recursive call
-			iterate(hash1, hash2, str, idx + 1, len, ok);
-		}
-	} else {
-		// Include all last letters and compare the hashes.
-		for (c = 0; c < strlen(letters) && *ok==0; ++c) {
-			str[idx] = letters[c];
-			MD5((byte *) str, strlen(str), hash2);
-			if(strncmp((char*)hash1, (char*)hash2, MD5_DIGEST_LENGTH) == 0){
-				printf("found: %s\n", str);
-				//print_digest(hash2);
-				*ok = 1;
-			}
-		}
-	}
+    printf("iterating string = %s, int = %d, length = %d, ok = %d\n", str, idx, len, *ok);
+    int c;
+    // 'ok' determines when the algorithm matches.
+    if(*ok) return;
+    if (idx < (len - 1)) {
+        // Iterate for all letter combination.
+        for (c = 0; c < strlen(letters) && *ok==0; ++c) {
+            str[idx] = letters[c];
+            // Recursive call
+            iterate(hash1, hash2, str, idx + 1, len, ok);
+        }
+    } else {
+        // Include all last letters and compare the hashes.
+        for (c = 0; c < strlen(letters) && *ok==0; ++c) {
+            str[idx] = letters[c];
+            MD5((byte *) str, strlen(str), hash2);
+            if(strncmp((char*)hash1, (char*)hash2, MD5_DIGEST_LENGTH) == 0){
+                printf("found: %s\n", str);
+                //print_digest(hash2);
+                *ok = 1;
+            }
+        }
+    }
 }
 
 /*
  * Convert hexadecimal string to hash byte.
 */
 void strHex_to_byte(char * str, byte * hash){
-	char * pos = str;
-	int i;
+    char * pos = str;
+    int i;
 
-	for (i = 0; i < MD5_DIGEST_LENGTH/sizeof *hash; i++) {
-		sscanf(pos, "%2hhx", &hash[i]);
-		pos += 2;
-	}
+    for (i = 0; i < MD5_DIGEST_LENGTH/sizeof *hash; i++) {
+        sscanf(pos, "%2hhx", &hash[i]);
+        pos += 2;
+    }
 }
 
 int main(int argc, char **argv) {
-	char str[MAX+1];
-	int lenMax = MAX;
-	int len;
-	int ok = 0, r;
-	char hash1_str[2*MD5_DIGEST_LENGTH+1];
-	byte hash1[MD5_DIGEST_LENGTH]; // password hash
-	byte hash2[MD5_DIGEST_LENGTH]; // string hashes
+    char str[MAX+1];
+    int lenMax = MAX;
+    int len;
+    int ok = 0, r;
+    char hash1_str[2*MD5_DIGEST_LENGTH+1];
+    byte hash1[MD5_DIGEST_LENGTH]; // password hash
+    byte hash2[MD5_DIGEST_LENGTH]; // string hashes
 
-	// Input:
-	r = scanf("%s", hash1_str);
+    // Input:
+    r = scanf("%s", hash1_str);
 
-	// Check input.
-	if (r == EOF || r == 0)
-	{
-		fprintf(stderr, "Error!\n");
-		exit(1);
-	}
+    // Check input.
+    if (r == EOF || r == 0)
+    {
+        fprintf(stderr, "Error!\n");
+        exit(1);
+    }
 
-	// Convert hexadecimal string to hash byte.
-	strHex_to_byte(hash1_str, hash1);
+    // Convert hexadecimal string to hash byte.
+    strHex_to_byte(hash1_str, hash1);
     
-	memset(hash2, 0, MD5_DIGEST_LENGTH);
-	//print_digest(hash1);
-	// Generate all possible passwords of different sizes.
-    printf("LenMax: %d\n");
+    memset(hash2, 0, MD5_DIGEST_LENGTH);
+    //print_digest(hash1);
+    // Generate all possible passwords of different sizes.
+    printf("LenMax: %d\n", lenMax);
     //MARK: tentar jogar o paralel for aqui
-	for(len = 1; len <= lenMax; len++){
-        printf("lenght = %d\n");
+    for(len = 1; len <= lenMax; len++){
+        printf("lenght = %d\n", len);
         //copia a string pra memoria se nao me engano
-		memset(str, 0, len+1);
+        memset(str, 0, len+1);
         
-		iterate(hash1, hash2, str, 0, len, &ok);
-	}
+        iterate(hash1, hash2, str, 0, len, &ok);
+    }
 }
